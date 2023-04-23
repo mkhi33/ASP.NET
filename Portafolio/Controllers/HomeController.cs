1095574e5ed6760a45962ba4808e059a023c0764
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-04-23 11:15:08 -0600

    Implementar inyección de dependencias

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -1,21 +1,23 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Portafolio.Models;
+using Portafolio.Services;
 
 namespace Portafolio.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly RepositorioProyectos repositorioProyectos;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, RepositorioProyectos repositorioProyectos)
     {
         _logger = logger;
+        this.repositorioProyectos = repositorioProyectos;
     }
 
     public IActionResult Index()
     {
-        var repositorioProyectos = new RepositorioProyectos();
         var proyectos = repositorioProyectos.ObtenerProyectos().Take(3).ToList();
         var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
 
