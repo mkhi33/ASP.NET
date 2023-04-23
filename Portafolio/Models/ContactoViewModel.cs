2d56f6f0ad47c62ddb9a62186be4d6bb65dbf13b
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-04-23 12:45:00 -0600

    Crear un modelo para la vista de contacto

---
+++ Portafolio/Models/ContactoViewModel.cs
@@ -0,0 +1,9 @@
+namespace Portafolio.Models
+{
+    public class ContactoViewModel
+    {
+        public static string Nombre { get; set; }
+        public static string Email { get; set; }
+        public static string Mensaje { get; set; }
+    }
+}
