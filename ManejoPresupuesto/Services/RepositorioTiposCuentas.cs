67b582f43362f9dfc564383149a45f01865f3042
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-04-25 17:05:41 -0600

    Implementar Método para crear TipoCuentas en la base de datos

--- ManejoPresupuesto/Services/RepositorioTiposCuentas.cs
+++ ManejoPresupuesto/Services/RepositorioTiposCuentas.cs
@@ -14,5 +14,13 @@ namespace ManejoPresupuesto.Services
             connectionString = configuration.GetConnectionString("defaultConnection");
         }
 
+        public void Crear(TipoCuenta tipoCuenta)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var id = connection.QuerySingle<int>($@"INSERT INTO TiposCuentas (Nombre, UsuarioId, Orden) 
+                                                    VALUES (@Nombre, @UsuarioId, 0);
+                                                    SELECT SCOPE_IDENTITY();", tipoCuenta);
+            tipoCuenta.Id = id;
+        }
     }
 }
