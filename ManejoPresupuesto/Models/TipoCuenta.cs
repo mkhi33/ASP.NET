d0b79e8d956099466d22c37b65f33312cec7cc9f
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-04-25 16:13:27 -0600

    Crear validación a nivel de modelo

--- ManejoPresupuesto/Models/TipoCuenta.cs
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -4,7 +4,7 @@ using ManejoPresupuesto.Validaciones;
 
 namespace ManejoPresupuesto.Models
 {
-    public class TipoCuenta
+    public class TipoCuenta : IValidatableObject
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "El campo {0} es requerido")]
@@ -14,5 +14,16 @@ namespace ManejoPresupuesto.Models
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
 
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Nombre != null && Nombre.Length > 0)
+            {
+                var primeraLetra = Nombre.ToString()[0].ToString();
+                if (primeraLetra != primeraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("La primera letra debe ser mayúscula", new string[] { nameof(Nombre) });
+                }
+            }
+        }
     }
 }
