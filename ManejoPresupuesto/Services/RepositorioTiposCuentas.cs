07a099eefaa7b827112bc1763d0e372c37210b16
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-04-25 17:04:07 -0600

    Crear repositorio TipoCuentas

---
+++ ManejoPresupuesto/Services/RepositorioTiposCuentas.cs
@@ -0,0 +1,18 @@
+
+using Dapper;
+using ManejoPresupuesto.Interfaces;
+using ManejoPresupuesto.Models;
+using Microsoft.Data.SqlClient;
+
+namespace ManejoPresupuesto.Services
+{
+    public class RepositorioTiposCuentas : IRepositorioTiposCuentas
+    {
+        private readonly string connectionString;
+        public RepositorioTiposCuentas(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("defaultConnection");
+        }
+
+    }
+}
