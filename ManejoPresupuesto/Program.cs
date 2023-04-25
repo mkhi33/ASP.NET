87727b45e18399c6aa3d85b03d1c9e994eb66114
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-04-25 17:06:32 -0600

    Hacer disponible el servicio RepositorioTiposCuentas para inyección de dependencias

--- ManejoPresupuesto/Program.cs
+++ ManejoPresupuesto/Program.cs
@@ -1,7 +1,11 @@
+using ManejoPresupuesto.Interfaces;
+using ManejoPresupuesto.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<IRepositorioTiposCuentas, RepositorioTiposCuentas>();
 
 var app = builder.Build();
 
