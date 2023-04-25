c6a13f3bbf0ef01ebc4382cb99a13145322baf2e
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-04-25 15:27:36 -0600

    Agregar validaión campo Nombre

--- ManejoPresupuesto/Models/TipoCuenta.cs
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -1,9 +1,13 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace ManejoPresupuesto.Models
 {
     public class TipoCuenta
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El campo {0} debe tener entre {2} y {1} caracteres")]
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
