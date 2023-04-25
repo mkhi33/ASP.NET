38741cce0581cca083959d0e2334367163a6adbd
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-04-25 16:07:26 -0600

    Crear validaciones personalizables (Primera letra mayuscula)

--- ManejoPresupuesto/Models/TipoCuenta.cs
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -1,5 +1,6 @@
 
 using System.ComponentModel.DataAnnotations;
+using ManejoPresupuesto.Validaciones;
 
 namespace ManejoPresupuesto.Models
 {
@@ -8,6 +9,7 @@ namespace ManejoPresupuesto.Models
         public int Id { get; set; }
         [Required(ErrorMessage = "El campo {0} es requerido")]
         [StringLength(50, MinimumLength = 3, ErrorMessage = "El campo {0} debe tener entre {2} y {1} caracteres")]
+        [PrimeraLetraMayuscula]
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
---
+++ ManejoPresupuesto/Validaciones/PrimeraLetraMayusculaAttribute.cs
@@ -0,0 +1,23 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Validaciones
+{
+    public class PrimeraLetraMayusculaAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if( value == null || string.IsNullOrEmpty(value.ToString()) )
+            {
+                return ValidationResult.Success;
+            }
+            var primeraLetra = value.ToString()[0].ToString();
+            if( primeraLetra != primeraLetra.ToUpper() )
+            {
+                return new ValidationResult("La primera letra debe ser mayúscula");
+            }
+            return ValidationResult.Success;
+        }
+        
+    }
+}
