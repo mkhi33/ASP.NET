8ab0057693ba6c88b89b7f374ccc99c0062ca9ad
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-04-25 16:38:26 -0600

    Hacer pruebas de conexión con la base de datos

--- ManejoPresupuesto/Controllers/TiposCuentasController.cs
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -1,12 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using ManejoPresupuesto.Models;
+using Microsoft.Data.SqlClient;
+using Dapper;
 
 namespace ManejoPresupuesto.Controllers
 {
     public class TiposCuentasController : Controller
     {
+        private readonly string connectionString;
+        private readonly ILogger<HomeController> _logger;
+        public TiposCuentasController(IConfiguration configuration, ILogger<HomeController> logger)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+            _logger = logger;
+        }
         public IActionResult Crear()
         {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var query = connection.Query("SELECT 1").FirstOrDefault();
+                _logger.LogInformation($"Query: {query}");
+            }
             return View();
         }
 
