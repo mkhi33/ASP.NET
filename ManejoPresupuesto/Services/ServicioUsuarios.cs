dc1ac0927b23728456a7d84c19df8809b2a5b53d
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-04-26 18:10:52 -0600

    Crear funcion temporal para obtener id de usuario

---
+++ ManejoPresupuesto/Services/ServicioUsuarios.cs
@@ -0,0 +1,13 @@
+using ManejoPresupuesto.Interfaces;
+namespace ManejoPresupuesto.Services
+
+{
+    public class ServicioUsuarios :IServicioUsuarios
+    {
+        public int ObtenerUsuarioId()
+        {
+            return 1;
+        }
+        
+    }
+}
