2207ca782530606a19557837ed9052da094e65cc
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-04-26 18:12:24 -0600

    Hacer disponible ServicioUsuario para la inyección de dependencias

--- ManejoPresupuesto/Program.cs
+++ ManejoPresupuesto/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddTransient<IRepositorioTiposCuentas, RepositorioTiposCuentas>();
+builder.Services.AddTransient<IServicioUsuarios, ServicioUsuarios>();
 
 var app = builder.Build();
 
