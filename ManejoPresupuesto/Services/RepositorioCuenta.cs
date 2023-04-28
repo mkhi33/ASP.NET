de056bfce78d252a118c29f29542c19685e30ace
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-04-28 10:23:55 -0600

    Crear función para buscar cuentas desde el repositorio

--- ManejoPresupuesto/Services/RepositorioCuenta.cs
+++ ManejoPresupuesto/Services/RepositorioCuenta.cs
@@ -6,22 +6,36 @@ using Microsoft.Data.SqlClient;
 
 namespace ManejoPresupuesto.Services
 {
-    public class RepositorioCuentas: IRepositorioCuentas
+    public class RepositorioCuentas : IRepositorioCuentas
     {
         private readonly string connectionString;
 
-        public RepositorioCuentas( IConfiguration configuration)
+        public RepositorioCuentas(IConfiguration configuration)
         {
             this.connectionString = configuration.GetConnectionString("DefaultConnection");
         }
 
-        public async Task Crear(Cuenta cuenta){
+        public async Task Crear(Cuenta cuenta)
+        {
             using var connection = new SqlConnection(connectionString);
             var id = await connection.QuerySingleAsync<int>(
                 @"INSERT INTO Cuentas (Nombre, TipoCuentaId, Descripcion, Balance) VALUES (@Nombre, @TipoCuentaId, @Descripcion, @Balance);
                 SELECT SCOPE_IDENTITY()", cuenta);
             cuenta.Id = id;
 
+        }
+        public async Task<IEnumerable<Cuenta>> Buscar(int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryAsync<Cuenta>(
+                @"SELECT Cuentas.Id, Cuentas.Nombre, Balance , tc.Nombre AS TipoCuenta 
+                FROM Cuentas 
+                    INNER JOIN TiposCuentas tc 
+                        ON tc.Id  = Cuentas.TipoCuentaId
+                    WHERE  tc.UsuarioId = @UsuarioId
+                    ORDER BY tc.Orden;",
+                new { usuarioId }
+            );
         }
     }
 }
