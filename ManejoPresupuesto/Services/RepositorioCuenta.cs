d555ebcacbccf51f3b9f778ee537f1dec70d7476
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-04-28 09:54:10 -0600

    Crear RepositorioCuentas, crear cuenta

---
+++ ManejoPresupuesto/Services/RepositorioCuenta.cs
@@ -0,0 +1,27 @@
+
+using Dapper;
+using ManejoPresupuesto.Interfaces;
+using ManejoPresupuesto.Models;
+using Microsoft.Data.SqlClient;
+
+namespace ManejoPresupuesto.Services
+{
+    public class RepositorioCuentas: IRepositorioCuentas
+    {
+        private readonly string connectionString;
+
+        public RepositorioCuentas( IConfiguration configuration)
+        {
+            this.connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task Crear(Cuenta cuenta){
+            using var connection = new SqlConnection(connectionString);
+            var id = await connection.QuerySingleAsync(
+                @"INSERT INTO Cuentas (Nombre, TipoCuentaId, Descripcion, Balance) VALUES (@Nombre, @TipoCuentaId, @Descripcion, @Balance);
+                SELECT SCOPE_IDENTITY()", cuenta);
+            cuenta.Id = id;
+
+        }
+    }
+}
