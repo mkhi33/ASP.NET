69d8600a50d5d8bb2e64f7d80314610fc5fe5345
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-04-28 10:39:14 -0600

    Agregar campo TipoCuenta

--- ManejoPresupuesto/Models/IndiceCuentasViewModel.cs
+++ ManejoPresupuesto/Models/IndiceCuentasViewModel.cs
@@ -4,6 +4,7 @@ namespace ManejoPresupuesto.Models
 {
     public class IndiceCuentasViewModel
     {
+        public string TipoCuenta { get; set; }
         public IEnumerable<Cuenta> Cuentas { get; set; }
         public decimal Balance => Cuentas.Sum( x => x.Balance);
     }
