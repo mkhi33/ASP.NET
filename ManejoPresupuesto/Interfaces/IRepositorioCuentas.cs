3ac6397cbc7b7853df4ede488d37177a58a7ec15
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-04-28 10:26:42 -0600

    Agregar método de Buscar a IRepositorioCuentas

--- ManejoPresupuesto/Interfaces/IRepositorioCuentas.cs
+++ ManejoPresupuesto/Interfaces/IRepositorioCuentas.cs
@@ -1,10 +1,12 @@
 using ManejoPresupuesto.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ManejoPresupuesto.Interfaces
 {
     public interface IRepositorioCuentas
     {
         Task Crear(Cuenta cuenta);
+        Task<IEnumerable<Cuenta>> Buscar(int usuarioId)
 
     }
 }
