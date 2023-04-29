41658bdb0e9ec4babdfadc5d03ec34d261a9a7c3
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-04-29 11:28:20 -0600

    Instalar AutoMapper

--- ManejoPresupuesto/Program.cs
+++ ManejoPresupuesto/Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddTransient<IRepositorioTiposCuentas, RepositorioTiposCuentas>();
 builder.Services.AddTransient<IServicioUsuarios, ServicioUsuarios>();
 builder.Services.AddTransient<IRepositorioCuentas, RepositorioCuentas>();
+builder.Services.AddAutoMapper(typeof(Program));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
