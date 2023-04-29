56339aeade01c84457df90a6f45b128cfb2c3ed0
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-04-29 12:56:22 -0600

    Cambios menores

--- ManejoPresupuesto/Models/Categoria.cs
+++ ManejoPresupuesto/Models/Categoria.cs
@@ -12,6 +12,7 @@ namespace ManejoPresupuesto.Models
         [Required(ErrorMessage = "El campo {0} es requerido")]
         [StringLength(maximumLength:50, ErrorMessage = "El nombre de la categoría no puede tener más de {1} caracteres")]
         public string Nombre { get; set; }
+        [Display(Name = "Tipo de operación")]
         public TipoOperacion TipoOperacionId { get; set; }
         public int UsuarioId { get; set; }
         
