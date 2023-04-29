e1c1326bfcdba3c1b6e50805ecdffdf10a2e75b8
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-04-29 14:30:56 -0600

    Actualización script DB

--- ManejoPresupuesto/SQL_SCRIPTS/DDL.sql
+++ ManejoPresupuesto/SQL_SCRIPTS/DDL.sql
@@ -7,8 +7,8 @@ CREATE SCHEMA dbo;
 
 -- DROP TABLE ManejoPresupuesto.dbo.TiposOperaciones;
 
-CREATE TABLE TiposOperaciones (
-	Id int IDENTITY(0,1) NOT NULL,
+CREATE TABLE ManejoPresupuesto.dbo.TiposOperaciones (
+	Id int IDENTITY(1,1) NOT NULL,
 	Descripcion nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 	CONSTRAINT TiposOperaciones_PK PRIMARY KEY (Id)
 );
@@ -20,7 +20,7 @@ CREATE TABLE TiposOperaciones (
 
 -- DROP TABLE ManejoPresupuesto.dbo.Usuarios;
 
-CREATE TABLE Usuarios (
+CREATE TABLE ManejoPresupuesto.dbo.Usuarios (
 	Id int IDENTITY(1,1) NOT NULL,
 	EmailNormalizado nvarchar(256) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 	PasswordHash nvarchar(MAX) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
@@ -35,13 +35,14 @@ CREATE TABLE Usuarios (
 
 -- DROP TABLE ManejoPresupuesto.dbo.Categorias;
 
-CREATE TABLE Categorias (
-	Id int IDENTITY(0,1) NOT NULL,
+CREATE TABLE ManejoPresupuesto.dbo.Categorias (
+	Id int IDENTITY(1,1) NOT NULL,
 	TipoOperacionId int NOT NULL,
 	UsuarioId int NOT NULL,
+	Nombre varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 	CONSTRAINT Categorias_PK PRIMARY KEY (Id),
-	CONSTRAINT Categorias_TipoOperaciones_FK FOREIGN KEY (TipoOperacionId) REFERENCES TiposOperaciones(Id) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT Categorias_Usuarios_FK FOREIGN KEY (UsuarioId) REFERENCES Usuarios(Id) ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT Categorias_TipoOperaciones_FK FOREIGN KEY (TipoOperacionId) REFERENCES ManejoPresupuesto.dbo.TiposOperaciones(Id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT Categorias_Usuarios_FK FOREIGN KEY (UsuarioId) REFERENCES ManejoPresupuesto.dbo.Usuarios(Id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
@@ -51,13 +52,13 @@ CREATE TABLE Categorias (
 
 -- DROP TABLE ManejoPresupuesto.dbo.TiposCuentas;
 
-CREATE TABLE TiposCuentas (
-	Id int IDENTITY(0,1) NOT NULL,
+CREATE TABLE ManejoPresupuesto.dbo.TiposCuentas (
+	Id int IDENTITY(1,1) NOT NULL,
 	Nombre nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 	UsuarioId int NOT NULL,
 	Orden int NOT NULL,
 	CONSTRAINT TiposCuentas_PK PRIMARY KEY (Id),
-	CONSTRAINT TiposCuentas_FK FOREIGN KEY (UsuarioId) REFERENCES Usuarios(Id) ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT TiposCuentas_FK FOREIGN KEY (UsuarioId) REFERENCES ManejoPresupuesto.dbo.Usuarios(Id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
@@ -67,14 +68,14 @@ CREATE TABLE TiposCuentas (
 
 -- DROP TABLE ManejoPresupuesto.dbo.Cuentas;
 
-CREATE TABLE Cuentas (
-	Id int IDENTITY(0,1) NOT NULL,
+CREATE TABLE ManejoPresupuesto.dbo.Cuentas (
+	Id int IDENTITY(1,1) NOT NULL,
 	Nombre nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 	TipoCuentaId int NOT NULL,
 	Balance decimal(18,2) NOT NULL,
 	Descripcion nvarchar(1000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	CONSTRAINT Cuentas_PK PRIMARY KEY (Id),
-	CONSTRAINT Cuentas_FK FOREIGN KEY (TipoCuentaId) REFERENCES TiposCuentas(Id) ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT Cuentas_FK FOREIGN KEY (TipoCuentaId) REFERENCES ManejoPresupuesto.dbo.TiposCuentas(Id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
@@ -84,8 +85,8 @@ CREATE TABLE Cuentas (
 
 -- DROP TABLE ManejoPresupuesto.dbo.Transacciones;
 
-CREATE TABLE Transacciones (
-	Id int IDENTITY(0,1) NOT NULL,
+CREATE TABLE ManejoPresupuesto.dbo.Transacciones (
+	Id int IDENTITY(1,1) NOT NULL,
 	UsuarioId int NOT NULL,
 	FechaTransaccion datetime NOT NULL,
 	Monto decimal(18,2) NOT NULL,
@@ -94,8 +95,8 @@ CREATE TABLE Transacciones (
 	CuentaId int NOT NULL,
 	CategoriaId int NOT NULL,
 	CONSTRAINT transacciones_PK PRIMARY KEY (Id),
-	CONSTRAINT Transacciones_Categorias_FK FOREIGN KEY (CategoriaId) REFERENCES Categorias(Id),
-	CONSTRAINT Transacciones_Cuentas_FK FOREIGN KEY (CuentaId) REFERENCES Cuentas(Id),
-	CONSTRAINT Transacciones_FK FOREIGN KEY (TipoTransaccionId) REFERENCES TiposOperaciones(Id) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT Transacciones_Usuarios_FK FOREIGN KEY (UsuarioId) REFERENCES Usuarios(Id) ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT Transacciones_Categorias_FK FOREIGN KEY (CategoriaId) REFERENCES ManejoPresupuesto.dbo.Categorias(Id),
+	CONSTRAINT Transacciones_Cuentas_FK FOREIGN KEY (CuentaId) REFERENCES ManejoPresupuesto.dbo.Cuentas(Id),
+	CONSTRAINT Transacciones_FK FOREIGN KEY (TipoTransaccionId) REFERENCES ManejoPresupuesto.dbo.TiposOperaciones(Id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT Transacciones_Usuarios_FK FOREIGN KEY (UsuarioId) REFERENCES ManejoPresupuesto.dbo.Usuarios(Id) ON DELETE CASCADE ON UPDATE CASCADE
 );
