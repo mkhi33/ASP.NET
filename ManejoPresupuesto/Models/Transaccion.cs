d1d295dc00c5a4a198bfd762cbba7bb5d6a5468a
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-04-30 09:27:12 -0600

    Crear el modelo de transacción

---
+++ ManejoPresupuesto/Models/Transaccion.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManejoPresupuesto.Models
+{
+    public class Transaccion
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime FechaTransaccion { get; set; } = DateTime.Today;
+        public decimal Monto { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Seleccione una categoría")]
+        public int CategoriaId { get; set; }
+        [StringLength(maximumLength: 1000, ErrorMessage = "La nota no puede tener más de {1} caracteres"))]
+        public string Nota { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Seleccione una Cuenta")]
+        public int CuentaId { get; set; }
+    }
+}
