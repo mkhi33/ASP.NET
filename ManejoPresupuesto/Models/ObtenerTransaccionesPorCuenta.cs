728c694780311519b21334da3fccefc45a25bee0
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-05-02 17:29:51 -0600

    Movimiento de cuentas

--- ManejoPresupuesto/Models/ObtenerTransaccionesPorCuenta.cs
+++ ManejoPresupuesto/Models/ObtenerTransaccionesPorCuenta.cs
@@ -8,6 +8,7 @@ namespace ManejoPresupuesto.Models
     public class ObtenerTransaccionesPorCuenta
     {
         public int UsuarioId { get; set; }
+        public int CuentaId { get; set; }
         public DateTime FechaInicio { get; set; }
         public DateTime FechaFin { get; set; }
     }
