5125c35e67dfcee70fb340ef4690a7792e164816
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-05-03 14:13:12 -0600

    Crear acción para pasar reporte a la vista

--- ManejoPresupuesto/Controllers/TransaccionesController.cs
+++ ManejoPresupuesto/Controllers/TransaccionesController.cs
@@ -24,9 +24,49 @@ namespace ManejoPresupuesto.Controllers
             this.mapper = mapper;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int mes, int año)
         {
-            return View();
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            DateTime fechaInicio;
+            DateTime fechaFin;
+            if (mes <= 0 || mes > 12 || año <= 1900)
+            {
+                var hoy = DateTime.Today;
+                fechaInicio = new DateTime(hoy.Year, hoy.Month, 1).AddMonths(1).AddDays(-1);
+            }
+            else
+            {
+                fechaInicio = new DateTime(año, mes, 1);
+            }
+            fechaFin = fechaInicio.AddMonths(1).AddDays(-1);
+            var parametro = new ParametroObtenerTransaccionesPorUsuario()
+            {
+                UsuarioId = usuarioId,
+                FechaInicio = fechaInicio,
+                FechaFin = fechaFin
+            };
+            var transacciones = await repositorioTransacciones.ObtenerPorUsuarioId(parametro);
+
+            var modelo = new ReporteTransaccionesDetalladas();
+            var transaccionesPorFecha = transacciones.OrderByDescending(x => x.FechaTransaccion)
+                                                    .GroupBy(x => x.FechaTransaccion)
+                                                    .Select(grupo => new ReporteTransaccionesDetalladas.TransaccionesPorFecha()
+                                                    {
+                                                        FechaTransaccion = grupo.Key,
+                                                        Transacciones = grupo.AsEnumerable()
+                                                    });
+            modelo.TransaccionesAgrupadas = transaccionesPorFecha;
+            modelo.FechaInicio = fechaInicio;
+            modelo.FechaFin = fechaFin;
+
+            ViewBag.mesAnterior = fechaInicio.AddMonths(-1).Month;
+            ViewBag.añoAnterior = fechaInicio.AddMonths(-1).Year;
+            ViewBag.mesPosterior = fechaInicio.AddMonths(1).Month;
+            ViewBag.añoPosterior = fechaInicio.AddMonths(1).Year;
+
+            ViewBag.urlRetorno = HttpContext.Request.Path + HttpContext.Request.QueryString;
+
+            return View(modelo);
         }
 
         [HttpGet]
