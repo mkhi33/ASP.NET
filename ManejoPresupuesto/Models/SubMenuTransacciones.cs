91761356ed4ed4850ecfbbc65633cc41874f62d3
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-05-05 16:46:14 -0600

    Crear submenu para reportes

---
+++ ManejoPresupuesto/Models/SubMenuTransacciones.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManejoPresupuesto.Models
+{
+    public enum SubMenuTransacciones
+    {
+        Diario,
+        Semanal,
+        Mensual,
+        ExcelReporte,
+        Calendario
+    }
+}
