16bcfa26ad6c861121cf9da783f90bbfe281c8ea
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-05-05 17:15:47 -0600

    Agregar campos Ingresos y Gastos

--- ManejoPresupuesto/Models/ResultadoObtenerPorSemana.cs
+++ ManejoPresupuesto/Models/ResultadoObtenerPorSemana.cs
@@ -10,6 +10,8 @@ namespace ManejoPresupuesto.Models
         public int Semana { get; set; }
         public decimal Monto { get; set; }
         public TipoOperacion TipoOperacionId { get; set; }
+        public decimal Ingresos { get; set; }
+        public decimal Gastos { get; set; }
 
     }
 }
