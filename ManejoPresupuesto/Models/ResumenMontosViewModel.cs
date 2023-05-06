4f22fd0f36d2ef033002b2a0606e915212c42561
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-05-05 18:31:27 -0600

    Crear modelo ResumenMontos para la vista

---
+++ ManejoPresupuesto/Models/ResumenMontosViewModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManejoPresupuesto.Models
+{
+    public class ResumenMontoViewModel
+    {
+        public decimal Ingresos { get; set; }
+        public decimal Gastos { get; set; }
+        public decimal Total { get; set; }
+    }
+}
