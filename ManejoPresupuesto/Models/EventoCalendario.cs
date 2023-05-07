ecc56ece85a029005f7bff9f9e82b7410217f798
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-05-07 13:46:30 -0600

    Crear modelo para el evento calendario

---
+++ ManejoPresupuesto/Models/EventoCalendario.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManejoPresupuesto.Models
+{
+    public class EventoCalendario
+    {
+        public string Title { get; set; }
+        public string Start { get; set; }
+        public string End { get; set; }
+        public string Color { get; set; }
+    }
+}
