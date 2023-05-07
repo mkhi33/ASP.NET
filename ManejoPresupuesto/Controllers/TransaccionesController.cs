74bf7ecc182cdbb8f6e262b2cccccb049e65cc7d
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-05-07 13:46:11 -0600

    Crear acción para obtener json de transacciones

--- ManejoPresupuesto/Controllers/TransaccionesController.cs
+++ ManejoPresupuesto/Controllers/TransaccionesController.cs
@@ -218,6 +218,30 @@ namespace ManejoPresupuesto.Controllers
             return View();
         }
 
+        public async Task<JsonResult> ObtenerTransaccionesCalendario(DateTime start, DateTime end)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var transacciones = await repositorioTransacciones.ObtenerPorUsuarioId(
+                new ParametroObtenerTransaccionesPorUsuario
+                {
+                    UsuarioId = usuarioId,
+                    FechaInicio = start,
+                    FechaFin = end
+                }
+            );
+            var eventosCalendario = transacciones.Select( transaccion => new EventoCalendario()
+                {
+                    Title = transaccion.Monto.ToString("n"),
+                    Start = transaccion.FechaTransaccion.ToString("yyyy-MM-dd"),
+                    End = transaccion.FechaTransaccion.ToString("yyyy-MM-dd"),
+                    Color = (transaccion.TipoOperacionId == TipoOperacion.Gasto) ? "red" : null
+
+                }
+            );
+            return Json(eventosCalendario);
+
+        }
+
         [HttpGet]
         public async Task<IActionResult> Crear()
         {
