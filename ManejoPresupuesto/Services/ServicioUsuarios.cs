4e33eb9f85d5d477c0d3fe2c1eeee074531f0ffb
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-05-18 14:01:44 -0600

    Obtener el id del usuario autenticado

--- ManejoPresupuesto/Services/ServicioUsuarios.cs
+++ ManejoPresupuesto/Services/ServicioUsuarios.cs
@@ -1,12 +1,28 @@
+using System.Security.Claims;
 using ManejoPresupuesto.Interfaces;
 namespace ManejoPresupuesto.Services
 
 {
     public class ServicioUsuarios :IServicioUsuarios
     {
+        private readonly HttpContext httpContextAccessor;
+
+        public ServicioUsuarios(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor.HttpContext;
+        }
         public int ObtenerUsuarioId()
         {
-            return 2;
+            if(httpContextAccessor.User.Identity.IsAuthenticated)
+            {
+                var idClain = httpContextAccessor.User.Claims.Where( x => x.Type == ClaimTypes.NameIdentifier).FirstOrDefault();
+                var id = int.Parse(idClain.Value);
+                return id;
+            }
+            else
+            {
+                throw new ApplicationException("El usuario no está autenticado");
+            }
         }
         
     }
