9b8ef477d29222113ce6b75ca5d21013199a0720
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-05-18 16:31:58 -0600

    Agregar valor por defecto a un dropdown list

--- ManejoPresupuesto/Controllers/TransaccionesController.cs
+++ ManejoPresupuesto/Controllers/TransaccionesController.cs
@@ -388,7 +388,10 @@ namespace ManejoPresupuesto.Controllers
         private async Task<IEnumerable<SelectListItem>> ObtenerCategorias(int usuarioId, TipoOperacion tipoOperacion)
         {
             var categorias = await repositorioCategorias.Obtener(usuarioId, tipoOperacion);
-            return categorias.Select(x => new SelectListItem(x.Nombre, x.Id.ToString()));
+            var resultado = categorias.Select(x => new SelectListItem(x.Nombre, x.Id.ToString())).ToList();
+            var opcionPorDefecto = new SelectListItem("--- Seleccione una categoría ---", "0", true);
+            resultado.Insert(0, opcionPorDefecto);
+            return resultado;
         }
 
         private async Task<IEnumerable<SelectListItem>> ObtenerCuentas(int usuarioId)
