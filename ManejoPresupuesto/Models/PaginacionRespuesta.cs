e4e96d2d428927f3f78864cf05558a0453886e0a
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-05-18 15:49:16 -0600

    Crear modelo para la respuesta de la paginación

---
+++ ManejoPresupuesto/Models/PaginacionRespuesta.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManejoPresupuesto.Models
+{
+    public class PaginacionRespuesta
+    {
+        public int Pagina { get; set; } = 1;
+        public int RecordsPorPagina { get; set; } = 10;
+        public int CantidadTotalRecords {get; set;}
+        public int CantidadTotalDePaginas => (int)Math.Ceiling((double)CantidadTotalRecords / RecordsPorPagina);
+        public string BaseURL { get; set;}
+    }
+
+    public class PaginacionRespuesta<T> : PaginacionRespuesta
+    {
+        public IEnumerable<T> Elementos { get; set; }
+    }
+}
