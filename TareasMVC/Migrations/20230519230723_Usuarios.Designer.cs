f95a4b2921726b7f932c1d1230d094bba75401b2
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-05-19 17:08:05 -0600

    Crear la migración para Usuarios

--- TareasMVC/Migrations/20230519230723_Usuarios.Designer.cs
+++ TareasMVC/Migrations/20230519230723_Usuarios.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TareasMVC;
 
@@ -11,9 +12,11 @@ using TareasMVC;
 namespace TareasMVC.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230519230723_Usuarios")]
+    partial class Usuarios
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
