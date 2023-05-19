a7916e61d5995ef3295c610c93c5c85d53194359
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-05-19 16:02:10 -0600

    Implementar Anotaciones de datos

--- TareasMVC/Entidades/Tarea.cs
+++ TareasMVC/Entidades/Tarea.cs
@@ -1,9 +1,13 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace TareasMVC.Entidades
 {
     public class Tarea
     {
        public int Id { get; set; } 
+       [StringLength(250)]
+       [Required]
        public string Titulo { get; set; }
        public string Descripcion { get; set; }
        public int Orden { get; set; }
