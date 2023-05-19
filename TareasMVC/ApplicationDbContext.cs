3ed33628c0a1dc9bbffffc1d262c7d2e90cfedb5
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-05-19 17:05:27 -0600

    Implementar IdentityDbContext :1 :1 exit

--- TareasMVC/ApplicationDbContext.cs
+++ TareasMVC/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using TareasMVC.Entidades;
 
 namespace TareasMVC
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
