f84ca671354a985e0efbe85a7dd86087edaa52f0
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-05-20 17:10:43 -0600

    Cargar la vista y registrar usuarios

---
+++ TareasMVC/Controllers/UsuariosController.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TareasMVC.Models;
+
+namespace TareasMVC.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public UsuariosController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Registro(RegistroViewModel modelo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(modelo);
+            }
+            var usuario = new IdentityUser
+            {
+                Email = modelo.Email,
+                UserName = modelo.Email
+            };
+            var resultado = await userManager.CreateAsync(usuario, password: modelo.Password);
+            if (resultado.Succeeded)
+            {
+                await signInManager.SignInAsync(usuario, isPersistent: true);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                foreach (var error in resultado.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description); 
+                }
+                return View(modelo);
+            }
+        }
+    }
+}
