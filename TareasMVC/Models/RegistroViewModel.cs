2afb4a8d79008f1f963d4f0a6c5f93f1e853900a
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-05-20 16:54:50 -0600

    Crear el modelo y la vista para registro de usuarios

---
+++ TareasMVC/Models/RegistroViewModel.cs
@@ -0,0 +1,15 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace TareasMVC.Models
+{
+    public class RegistroViewModel
+    {
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [EmailAddress(ErrorMessage = "El campo debe ser un correo electrónico valido")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
