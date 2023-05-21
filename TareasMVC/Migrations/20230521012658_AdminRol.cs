cdcb8bf0564472c944552035430e267963d49807
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-05-20 19:30:35 -0600

    Crear migración para AdminRol

---
+++ TareasMVC/Migrations/20230521012658_AdminRol.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TareasMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminRol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            IF NOT EXISTS (SELECT Id FROM AspNetRoles WHERE Id = '8b167156-5bbc-4a5a-9e32-4f4a442def27')
+            BEGIN 
+
+                INSERT AspNetRoles(Id, [Name], [NormalizedName]) VALUES 
+                ('8b167156-5bbc-4a5a-9e32-4f4a442def27', 'admin', 'ADMIN');	
+                
+            END
+");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE AspNetRoles WHERE Id = '8b167156-5bbc-4a5a-9e32-4f4a442def27'");
+        }
+    }
+}
