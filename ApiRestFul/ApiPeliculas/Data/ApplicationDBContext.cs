34a4dc0dac43051e97b649a48a33d7c4777580c2
Amilcar Rodriguez <aarodriguezv@unah.hn>
2024-02-04 19:00:52 -0600

    Crear Configuración DBContext para EFC

---
+++ ApiRestFul/ApiPeliculas/Data/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+
+using ApiPeliculas.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace ApiPeliculas.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext( DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        // Agregar los modelos Aquí
+        public DbSet<Categoria> Categoria {get; set;}
+    }
+}
